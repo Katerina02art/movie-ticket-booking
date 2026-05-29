5ffa8a056298bfafdab8ba851a3ec2c164b7f50a
Katerina02art <artemovaekaterina317@gmail.com>
2026-05-29 20:55:48 +0400

    тысячу и один раз переделанный полностью диплом

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/shfe-diplom/",
+  plugins: [react()],
+
+
+  esbuild: {
+    loader: 'jsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      }
+    }
+  }
+});
+
+
+
