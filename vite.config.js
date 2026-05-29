1d088781bdfde3f7dfd2f5d3e838a72a43ad76da
Katerina02art <artemovaekaterina317@gmail.com>
2026-05-29 20:59:49 +0400

    еще одно изменение

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/shfe-diplom/",
   plugins: [react()],
-
-
-  esbuild: {
-    loader: 'jsx',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      }
-    }
-  }
+  base: "/movie-ticket-booking/",
 });
-
-
-
