f40621b9e52c2488bba565abaef2b144935e60ca
Katerina02art <artemovaekaterina317@gmail.com>
2026-07-04 20:35:33 +0400

    финал2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import HomePage from "./components/HomePage.jsx";
 import Login from "./components/Login.jsx";
 import AdminPage from "./components/AdminPage.jsx";
@@ -6,6 +8,8 @@ import Hall from "./components/Hall.jsx";
 import Payment from "./components/Payment.jsx";
 import Ticket from "./components/Ticket.jsx";
 import NotFound from "./components/NotFound.jsx";
+
+// Подключаем стили
 import "./css/client.css";
 import "./css/admin.css";
 import "./css/popup.css";
@@ -15,12 +19,11 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/:id" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminPage />} />
         <Route path="/hall/:id" element={<Hall />} />
         <Route path="/payment/:id" element={<Payment />} />
         <Route path="/ticket/:id" element={<Ticket />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </HashRouter>
